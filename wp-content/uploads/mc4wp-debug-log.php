8048ddc291d0a76623780c7e38feb4d9047d2add
Tuan Pham <tuantrylook@gmail.com>
2018-12-05 11:59:12 +0700

    aa

---
+++ wp-content/uploads/mc4wp-debug-log.php
@@ -0,0 +1,5 @@
+<?php exit; ?>
+[2018-12-03 01:16:52] INFO: Form 235 > Submitted with errors: no_lists_selected
+[2018-12-03 01:24:07] INFO: Form 235 > Successfully subscribed aaa@gm***.com
+[2018-12-04 07:36:56] INFO: Form 235 > Successfully subscribed anth***@gm***.com
+[2018-12-04 07:38:53] INFO: Form 235 > Successfully subscribed anth***@gm***.com
